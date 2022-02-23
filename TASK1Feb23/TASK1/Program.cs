17a2814de10d4a66eab8f1360c4da72a9c292f72
CeyhunAxundzada <98798456+CeyhunAxundzada@users.noreply.github.com>
2022-02-24 00:51:01 +0400

    Add files via upload

---
+++ TASK1Feb23/TASK1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace TASK1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ad ve soyad daxil et");
+            string ad_soyad = Console.ReadLine();
+
+            Console.WriteLine("Group n daxil et");
+            string qrup = Console.ReadLine();
+
+            Console.WriteLine("Yas daxil et");
+            string yas = Console.ReadLine();
+            int yas1 = int.Parse(yas);
+            Students student = new Students(ad_soyad, qrup, yas1);
+        }
+    }
+}
